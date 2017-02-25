e93daf6af3dbc2db223a5645153a69e01284444b
akshaym91 <akshaym91@gmail.com>
2017-02-25 12:32:36 +0530

    Completed the Cat clicker premium with knockoutJS as the underlying library. Refactored the code to make it simple and straight.

--- js/script.js
+++ js/script.js
@@ -1,80 +1,39 @@
-var ViewModel = function() {
+var Cat = function(cat) {
     this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.name = ko.observable(cat.name);
+    this.imgSrc = ko.observable(cat.image);
+    this.catlevel = ko.computed(function() {
+        var level;
+        if (this.clickCount() < 10) {
+            level = 'Beginner';
+        } else if (this.clickCount() >=10 && this.clickCount() < 20) {
+            level = 'Midlevel';
+        } else if (this.clickCount() >=20 && this.clickCount() < 30) {
+            level = 'Professional';
+        } else {
+            level = 'Cult';
+        }
+        return level;
+    }, this);
+};
+
+var ViewModel = function() {
+    var self = this;
+    this.catlist = ko.observableArray([]);
+
+    cats.forEach(function(cat){
+        self.catlist.push(new Cat(cat));
+    });
+
+    this.currentCat = ko.observable(self.catlist()[0]);
+
+    this.incrementCounter = function() {
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    };
+
+    this.setCat = function(cat) {
+        self.currentCat(cat);
+    };
 };
 
 ko.applyBindings(new ViewModel());
-
-var model = {
-    clicked: [0, 0, 0, 0, 0, 0],
-    init: function() {
-        this.clicked = [0, 0, 0, 0, 0, 0];
-    },
-    increment: function(index) {
-        this.clicked[index]++;
-    },
-    getClicks: function(index) {
-        return this.clicked[index];
-    }
-};
-var list_view = {
-    init: function() {
-        $.ajax({
-            url: 'mock-data.json',
-            success: function(result) {
-                $.each(result.cats, function(index, cat) {
-                    $('<a/>', {
-                        'id': 'cat' + (index),
-                        'class': 'list-group-item',
-                        'html': cat.name,
-                        'onclick': 'octopus.onNameClick(id)'
-                    }).appendTo('#cat-list');
-                });
-            }
-        }).error(function(e) {
-            console.log(e);
-        });
-    }
-};
-var canvas_view = {
-    init: function() {
-        $('.cat').remove();
-        $('.counter').remove();
-    },
-    display: function(index) {
-        $('<img/>', {
-            'id': 'cat' + index + '-img',
-            'class': 'cat',
-            'src': 'img/cat' + index + '.jpg',
-            'onclick': 'octopus.onCatClick(id)'
-        }).appendTo('#cat-canvas').fadeIn();
-        $('.cat-name').text('Cat '+ index);
-        $('.click-counter').text(octopus.getMyClicks(index));
-    },
-    update: function(index) {
-        $('.click-counter').text(octopus.getMyClicks(index));
-    }
-};
-var octopus = {
-    onCatClick: function(catid) {
-        var catname = catid.split('-')[0];
-        var catindex = parseInt(catname.split('')[catname.length - 1]);
-        model.increment(catindex);
-        canvas_view.update(catindex);
-    },
-    onNameClick: function(catid) {
-        var catindex = parseInt(catid.split('')[catid.length - 1]);
-        canvas_view.init();
-        canvas_view.display(catindex);
-    },
-    getMyClicks: function(index) {
-        // debugger;
-        return model.getClicks(index);
-    }
-};
-
-$(document).ready(function() {
-    canvas_view.init();
-    list_view.init();
-});
---
+++ mock-data.js
@@ -0,0 +1,19 @@
+var cats = [{
+    name: "Biba",
+    image: "img/cat1.jpg"
+}, {
+    name: "Cica",
+    image: "img/cat2.jpg"
+}, {
+    name: "Dida",
+    image: "img/cat3.jpg"
+}, {
+    name: "Fifa",
+    image: "img/cat4.jpg"
+}, {
+    name: "Giga",
+    image: "img/cat5.jpg"
+}, {
+    name: "Hiha",
+    image: "img/cat6.jpg"
+}];
